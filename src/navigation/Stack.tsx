5d865601d97393f3ccff571658026e5b7883514e
Alizade00 <turkanlizad430@gmail.com>
2023-12-16 14:37:42 +0400

    welcome-screen

--- src/navigation/Stack.tsx
+++ src/navigation/Stack.tsx
@@ -1,12 +1,14 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import { Welcome } from "../views/Welcome";
+import { SignIn } from "../views/SignIn";
 
 const Stack = createStackNavigator();
 
 export const StackNavigator = () => {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Welcome" component={Welcome} />
+            <Stack.Screen options={{headerShown:false}} name="Welcome" component={Welcome} />
+            <Stack.Screen name="SignIn" component={SignIn} />
         </Stack.Navigator>
     )
 }
